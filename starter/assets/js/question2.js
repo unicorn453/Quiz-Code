da94dda719c97defe488efc225d4408f9d11100e
unicorn453 <kira.ruseva@gmail.com>
2024-01-16 20:05:59 +0200

    added new js files

--- starter/assets/js/question2.js
+++ starter/assets/js/question2.js
@@ -1,14 +1,14 @@
 const quizQuestions = [
   {
-    question: "What is the capital of France?",
-    options: ["Berlin", "Madrid", "Paris", "Rome"],
-    correctAnswer: "Paris",
+    question: "What is the capital of Germany?",
+    options: ["Madrid", "Berlin", "Athens", "Skopje"],
+    correctAnswer: "Berlin",
   },
 ];
 const title = document.getElementById("title");
 title.innerHTML = `
-${quizQuestions[0].question}
-`;
+  ${quizQuestions[0].question}
+  `;
 const optionsListElement = document.getElementById("optionsList");
 var optionCities = quizQuestions.map((question) => question.options);
 var listItem0 = document.createElement("button");
@@ -27,12 +27,3 @@ optionsListElement.appendChild(listItem0);
 optionsListElement.appendChild(listItem1);
 optionsListElement.appendChild(listItem2);
 optionsListElement.appendChild(listItem3);
-
-//This code implies various ways for execution that do not include repetitive code but did not work.
-// for (const city of optionCities) {
-//   for (let i = 0; i < optionCities.length; i++) {
-//     // document.createElement("li");
-//     listItem.textContent = city[i];
-//   }
-// }
-// optionsListElement.appendChild(listItem);
